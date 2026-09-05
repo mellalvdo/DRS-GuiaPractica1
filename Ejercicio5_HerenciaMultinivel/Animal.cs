84cb75d14883eaf36ea04ff8a291447c186bc1a4
mellalvdo <melalvrdo@gmail.com>
2026-09-05 14:19:28 -0600

    Agregar clase Animal del ejercicio 5

---
+++ Ejercicio5_HerenciaMultinivel/Animal.cs
@@ -0,0 +1,12 @@
+namespace Ejercicio5_HerenciaMultinivel;
+
+/// <summary>
+/// Clase base de la jerarquía de herencia multinivel.
+/// </summary>
+public class Animal
+{
+    public virtual void HacerSonido()
+    {
+        Console.WriteLine("El animal hace un sonido.");
+    }
+}
