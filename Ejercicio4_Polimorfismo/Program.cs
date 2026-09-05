7bf36acde4811db45024707ae8375d1126c0c19f
mellalvdo <melalvrdo@gmail.com>
2026-09-05 14:37:26 -0600

    Usar Main tradicional en ejercicio 4

--- Ejercicio4_Polimorfismo/Program.cs
+++ Ejercicio4_Polimorfismo/Program.cs
@@ -1,18 +1,28 @@
-using Ejercicio4_Polimorfismo;
+using System;
 
-Console.WriteLine("=== EJERCICIO 4: POLIMORFISMO ===\n");
-
-// Las variables son de tipo Animal, pero contienen objetos de clases derivadas.
-Animal animal1 = new Perro();
-Animal animal2 = new Gato();
-
-animal1.HacerSonido();
-animal2.HacerSonido();
-
-Console.WriteLine("\nRecorriendo diferentes animales mediante la misma referencia base:");
-Animal[] animales = { new Perro(), new Gato() };
-
-foreach (Animal animal in animales)
+namespace Ejercicio4_Polimorfismo
 {
-    animal.HacerSonido();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== EJERCICIO 4: POLIMORFISMO ===\n");
+
+            // Las referencias son de tipo Animal, pero apuntan a objetos distintos.
+            Animal animal1 = new Perro();
+            Animal animal2 = new Gato();
+
+            animal1.HacerSonido();
+            animal2.HacerSonido();
+
+            Console.WriteLine("\nRecorriendo diferentes animales mediante una referencia común:");
+
+            Animal[] animales = { new Perro(), new Gato() };
+
+            foreach (Animal animal in animales)
+            {
+                animal.HacerSonido();
+            }
+        }
+    }
 }
